7638f322e8ee8acce75726367126e3cbe646eba7
이효상 <paul950104@gmail.com>
2023-11-26 03:17:40 +0900

    init

---
+++ springarchitect/src/main/java/com/spring/springarchitect/SpringarchitectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springarchitect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringarchitectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringarchitectApplication.class, args);
+	}
+
+}
---
+++ springarchitect/src/test/java/com/spring/springarchitect/SpringarchitectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springarchitect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringarchitectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
